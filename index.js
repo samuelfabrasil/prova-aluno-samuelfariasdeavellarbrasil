5c26f601c3394eed24bca5be765ad9779daf405b
samuelfabrasil <samuel.farias.avellar07@aluno.ifce.edu.br>
2021-12-13 14:15:04 -0300

    adicionando README

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bp = require('body-parser')
 const app = express()
-const port = 3000
+const port = 3300
 
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
